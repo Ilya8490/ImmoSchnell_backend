4c206c10956fe484635cb11de3921ef7b983fbbc
Ilya <zubrik1984@yahoo.com>
2024-02-08 11:41:43 +0100

    added users.js for seeding

--- seed/seed_db_users.js
+++ seed/seed_db_users.js
@@ -1,32 +1,25 @@
 import mongoose from "mongoose";
 import User from "../models/userModel.js";
-import { faker } from "@faker-js/faker";
+import { users } from "./users.js";
 import bcrypt from "bcryptjs";
 import "dotenv/config";
 
 (async () => {
-  try {
-    await mongoose.connect(process.env.DB_URI);
-
-    await User.deleteMany();
-    console.log("Users purged");
-
-    const users = Array.from({ length: 5 }, async () => ({
-      firstName: faker.name.firstName(),
-      lastName: faker.name.lastName(),
-      email: faker.internet.exampleEmail(),
-      password: await bcrypt.hash(faker.internet.password(), 8),
-      dateOfBirth: faker.date.between('1950-01-01', '2003-12-31'),
-      phone: faker.phone.phoneNumber(),
-    }));
-
-    const resolvedUsers = await Promise.all(users);
-
-    await User.create(resolvedUsers);
-    console.log("Users data seeded successfully!");
-  } catch (error) {
-    console.error("Error while seeding data:", error);
-  } finally {
-    mongoose.connection.close();
-  }
-})();
+    try {
+      await mongoose.connect(process.env.DB_URI);
+  
+      await User.deleteMany();
+      console.log("Users purged");
+  
+      for (let user of users) {
+        user.password = await bcrypt.hash(user.password, 8);
+      }
+  
+      await User.create(users);
+      console.log("Users data seeded successfully!");
+    } catch (error) {
+      console.error("Error while seeding data:", error);
+    } finally {
+      mongoose.connection.close();
+    }
+  })();
---
+++ seed/users.js
@@ -0,0 +1,19 @@
+export const users = [
+    {
+      firstName: "John",
+      lastName: "Doe",
+      email: "johndoe@example.com",
+      password: "password123",
+      dateOfBirth: new Date('1980-01-01'),
+      phone: "+1234567890"
+    },
+    {
+      firstName: "Jane",
+      lastName: "Smith",
+      email: "janesmith@example.com",
+      password: "password123",
+      dateOfBirth: new Date('1985-02-02'),
+      phone: "+1098765432"
+    }
+    
+  ];
